664265fb6b76216ca52f5251e4bb38370cbdecf6
kixkz <e.moskov@abv.bg>
2022-08-04 16:22:25 +0300

    Added CSharp Fundamentals: Regular Expressions - Exercise

---
+++ CSharp Fundamentals/Homeworks-And-Labs/Regular Expressions - Exercise/01. Furniture/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace _01._Furniture
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            string input;
+            string regex = @">>(?<name>[A-Za-z\s]+)<<(?<price>\d+\.?([0-9]+)?)!(?<quantity>\d+)";
+            List<string> furnitureList = new List<string>();
+            double sum = 0;
+
+            while ((input = Console.ReadLine()) != "Purchase")
+            {
+                Match matches = Regex.Match(input, regex, RegexOptions.IgnoreCase);
+                if (matches.Success)
+                {
+                    string name = matches.Groups["name"].Value;
+                    double price = double.Parse(matches.Groups["price"].Value);
+                    int quantity = int.Parse(matches.Groups["quantity"].Value);
+
+                    furnitureList.Add(name);
+                    sum += quantity * price;
+                } 
+            }
+
+            Console.WriteLine("Bought furniture:");
+            foreach (var furniture in furnitureList)
+            {
+                Console.WriteLine(furniture);
+            }
+
+            Console.WriteLine($"Total money spend: {sum:f2}");
+        }
+    }
+}
